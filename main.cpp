c42b3e7ff55bb0a1bc30d4146457a1b000252f37
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-10-05 21:37:45 -0700

    Cleared up main to rewrite for Lab 9, finished addReview function, need to run to check for compilation/output at this point

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 // COMSC210 | Lab 19 | Tanmayee Chalamalasetti
 // IDE Used: VS Code
 
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -25,7 +28,17 @@ class Movie {
     head = nullptr;
   }
 
-  void addReview(float rating, string comment) {}
+  // addReview
+  // arguments: float rating, string comment
+  // returns: nothing
+  void addReview(float rating, string comment) {
+    ReviewNode* newNode = new ReviewNode;
+    newNode->rating = rating;
+    newNode->comment = comment;
+    newNode->next = head;
+    head = newNode;
+  }
+
   // displayReview outputs the review
   // arguments: ReviewNode* head
   // returns: nothing
@@ -57,93 +70,6 @@ class Movie {
       cout << "No reviews to average.\n";
     }
   }
+};
 
-}
-
-int main() {
-  int choice;  // choice for head or tail
-  cout << "Which linked list method should we use?\n";
-  cout << "   [1] New nodes are added at the head of the linked list\n";
-  cout << "   [2] New nodes are added at the tail of the linked list\n";
-  cout << "   Choice: ";
-  cin >> choice;
-
-  // input validation if someone puts in another value other than 1 or 2
-  while (choice != 1 && choice != 2) {
-    cout << "ERROR: Please enter 1 or 2.\nChoice: ";
-    cin >> choice;
-  }
-
-  // sets true to 1
-  bool addToHead = (choice == 1);
-
-  // sets head and tail to null pointers
-  ReviewNode* head = nullptr;
-  ReviewNode* tail = nullptr;
-
-  // initialized some variables for input validation and checking for more
-  // reviews
-  char multiple = 'y';
-  float rating;
-  string comment;
-
-  while (multiple == 'y' || multiple == 'Y') {
-    cout << "Enter review rating 0-5: ";
-    cin >> rating;
-    while (rating > max_rating || rating < min_rating) {
-      cout << "ERROR: Rating must be between 0.0 and 5.0. Try again: ";
-      cin >> rating;
-    }
-    cout << "Enter review comments: ";
-    cin >> comment;
-
-    // created a new Node
-    ReviewNode* newNode = new ReviewNode;
-    newNode->rating = rating;
-    newNode->comment = comment;
-    newNode->next = nullptr;
-
-    // checks if addToHead is true or 1 and adds to the head
-    if (addToHead == true) {
-      newNode->next = head;
-      head = newNode;
-      if (tail == nullptr) {
-        tail = newNode;
-      }
-      // else (if it is 2) it adds to the tail
-    } else {
-      if (head == nullptr) {
-        head = newNode;
-        tail = newNode;
-      } else {
-        tail->next = newNode;
-        tail = newNode;
-      }
-    }
-
-    cout << "Enter another review? Y/N: ";
-    cin >> multiple;
-    cin.ignore();
-
-    // I put both lower case and upper case Y
-    while (multiple != 'y' && multiple != 'Y' && multiple != 'n' &&
-           multiple != 'N') {
-      cout << "ERROR: Please enter Y or N: \n";
-      cin >> multiple;
-      cin.ignore();
-    }
-  }
-
-  // call displayReview to display the review
-  displayReview(head);
-
-  // deleting nodes
-  ReviewNode* current = head;
-  while (current != nullptr) {
-    ReviewNode* temp = current;
-    current = current->next;
-    delete temp;
-  }
-
-  return 0;
-}
+int main() { return 0; }
